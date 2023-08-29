07419fa0fd3e132dc57e6ede36ac0f8924da45de
Sugakesshaa <sugakesshaa@gmail.com>
2023-08-29 18:28:32 +0800

    Revert "nuke opiss bs [1/?]"
    This reverts commit a4c51d4307f716f5e6481f757f4561f96f92e7bf.

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -2141,6 +2141,10 @@ static int __init init_sel_fs(void)
 		selinux_null.dentry = NULL;
 	}
 
+#ifdef OPLUS_FEATURE_SELINUX_CONTROL_LOG
+	init_denied_proc();
+#endif /* OPLUS_FEATURE_SELINUX_CONTROL_LOG */
+
 	return err;
 }
 
